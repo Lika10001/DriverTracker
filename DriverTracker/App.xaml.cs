eff338c593fcc0696879aa39d394399a6862054a
Lika10001 <shimanskaya.anzhela@mail.ru>
2024-06-22 17:33:29 +0300

    Added SighUp, SighIn pages, raw MainPage, DriverInfoPage, AddDriverPage

---
+++ DriverTracker/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DriverTracker;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DriverTracker/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DriverTracker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
