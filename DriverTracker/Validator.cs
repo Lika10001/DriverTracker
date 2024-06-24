862bb03a8bf2ead34d9e6e83fa39c45c116fd472
Lika10001 <shimanskaya.anzhela@mail.ru>
2024-06-24 17:36:38 +0300

    Added class validator for input data

---
+++ DriverTracker/Validator.cs
@@ -0,0 +1,18 @@
+namespace DriverTracker;
+//this class validates data according to its type.
+//Methods return true, if data is correct, false - data is not correct.  
+public static class Validator
+{
+    private const int PasswordLength = 6;
+    private const int LoginLength = 6;
+        
+    public static bool CheckPassword(string data)
+    {
+        return ((data.Length > PasswordLength) && (string.IsNullOrWhiteSpace(data)));
+    }
+    
+    public static bool CheckLogin(string data)
+    {
+        return ((data.Length > LoginLength) && (string.IsNullOrWhiteSpace(data)));
+    }
+}
